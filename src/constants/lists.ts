47499ecdeb917ef5b514ca3a919c6636f6631bb2
wwl <wwl0115@163.com>
2021-03-11 21:03:25 +0800

    initial

---
+++ src/constants/lists.ts
@@ -0,0 +1,5 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL =
+  'https://raw.githubusercontent.com/MMMSwap/mmmswap-front/master/src/default-tokens.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
